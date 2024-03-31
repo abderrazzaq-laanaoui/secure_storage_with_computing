1bb379575907e1ab03f79b48dbe919cef21bff23
Raharinosy Noel <raharinosy.e2201402@etud.univ-ubs.fr>
2024-03-31 16:24:00 +0200

    Update Client.py

--- client/Client.py
+++ client/Client.py
@@ -1,3 +1,4 @@
+import os
 """import mysql.connector
 
 # Se connecter à la base de données
@@ -13,43 +14,68 @@ def decrypt_data(encrypted_data):
     # Utiliser le middleware pour déchiffrer les données
     decrypted_data = middleware.decrypt(encrypted_data)
     return decrypted_data
-
+    
+def clear_console():
+    # Effacer la console selon le système d'exploitation
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
 # Fonction pour ajouter une valeur
 def add_value():
+	print("Vous avez choisi d'ajouter une valeur dans la base de données")
 	value = int(input("Choisissez une valeur a ajouter:"))
 	# Fonction add
 	print("Valeur ajoutée, l'id est ")
 
 def show_id():
-	print("Voici la liste des IDs:")
-	last_id = 4
-	#GetID
-	id_list = []  
-	for i in range(1, last_id + 1):
-		id_list.append(str(i))  
-	print("[" + ", ".join(id_list) + "]")
+    print("Voici la liste des IDs:")
+    last_id = 4
+    # GetID
+    id_list = []  
+    for i in range(1, last_id + 1):
+        id_list.append(i)  
+    print(id_list)  # Affiche la liste d'IDs
+    return id_list
+
 
 
 # Fonction pour comparer deux valeurs
 def compare_values():
-	show_id()
-	id_1 = int(input("Choisissez l'id 1:"))
-	id_2 = int(input("Choisissez l'id 2:"))
+	print("Vous avez choisi de comparer deux valeur dans la base de données")
+	lst = show_id()
+	while True:
+		id_1 = int(input("Choisissez l'id 1:"))
+		id_2 = int(input("Choisissez l'id 2:"))
+		# Vérifier si les IDs choisis sont présents dans la liste lst
+		if id_1 in lst and id_2 in lst:
+		    print("Les IDs choisis sont valides.")
+		    break  # Sortir de la boucle while une fois que les IDs sont valides
+		else:
+		    print("Au moins l'un des IDs choisis n'est pas valide. Veuillez choisir des IDs valides.")
 
 # Fonction pour additionner deux valeurs
 def sum_values():
-	show_id()
-	id_1 = int(input("Choisissez l'id 1:"))
-	id_2 = int(input("Choisissez l'id 2:"))
+	print("Vous avez choisi d'additionner deux valeur")
+	lst = show_id()
+	while True:
+		id_1 = int(input("Choisissez l'id 1:"))
+		id_2 = int(input("Choisissez l'id 2:"))
+		# Vérifier si les IDs choisis sont présents dans la liste lst
+		if id_1 in lst and id_2 in lst:
+			print("Les IDs choisis sont valides.")
+			break  # Sortir de la boucle while une fois que les IDs sont valides
+		else:
+	        	print("Au moins l'un des IDs choisis n'est pas valide. Veuillez choisir des IDs valides.")
 	
-
+	
+	
+clear_console() 
 # Boucle principale pour le menu
 while True:
-    print("Votre choix:")
-    print("1. Ajouter une valeur")
-    print("2. Comparer deux valeurs")
-    print("3. Additionner deux valeurs")
-    print("4. Quitter")
+    print("Votre choix")
+    print("   1. Ajouter une valeur")
+    print("   2. Comparer deux valeurs")
+    print("   3. Additionner deux valeurs")
+    print("   4. Quitter")
 
     choice = int(input("Choisissez une option: "))
 
