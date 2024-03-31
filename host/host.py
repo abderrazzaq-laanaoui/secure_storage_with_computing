d411f3df2a9009557d90190643952e9c3c5575af
Raharinosy Noel <raharinosy.e2201402@etud.univ-ubs.fr>
2024-03-31 17:48:42 +0200

    Finish host.py

--- host/host.py
+++ host/host.py
@@ -1,6 +1,8 @@
 import mysql.connector
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import List, Dict
+import json
 
 class DatabaseHost:
     def __init__(self, host, user, password, database):
@@ -27,7 +29,7 @@ class DatabaseHost:
             self.connection.close()
             print("Déconnexion de la base de données.")
 
-    def read(self, table, columns="*", condition=None):
+    def read(self, table, columns="*", condition=None) -> List[Dict]:
         cursor = self.connection.cursor()
         query = f"SELECT {columns} FROM {table}"
         if condition:
@@ -35,16 +37,24 @@ class DatabaseHost:
         cursor.execute(query)
         data = cursor.fetchall()
         cursor.close()
-        return data
 
-    def write(self, table, data):
-        cursor = self.connection.cursor()
-        placeholders = ', '.join(['%s'] * len(data))
-        columns = ', '.join(data.keys())
-        query = f"INSERT INTO {table} ({columns}) VALUES ({placeholders})"
-        cursor.execute(query, list(data.values()))
-        self.connection.commit()
-        cursor.close()
+        # Convertir les données en une liste de dictionnaires
+
+        if columns == "*":
+            # Si toutes les colonnes sont sélectionnées, obtenir les noms de colonnes à partir du curseur
+            columns = [col[0] for col in cursor.description]
+        for row in data:
+            row_data = {}
+            for i, value in enumerate(row):
+                # Convertir les valeurs bytearray en chaînes de caractères
+                if isinstance(value, bytearray):
+                    row_data[columns[i]] = value.decode('utf-8')
+                else:
+                    row_data[columns[i]] = value
+            return row_data
+        
+        return None
+
 
 app = FastAPI()
 db_host = DatabaseHost(host="localhost", user="root", password="Ensibs@log2024", database="secret_db")
@@ -54,9 +64,12 @@ class Item(BaseModel):
     arithmetic_value: str
     binary_value: str
 
-@app.get("/")
-def read_root():
-    data = db_host.read(table="secret_data")
+@app.get("/{id}")
+def read_root(id: int):
+    condition = f"id = {id}"  # Condition to select the record with the specified id
+    data = db_host.read(table="secret_data", condition=condition)
+    if not data:
+        raise HTTPException(status_code=404, detail="Record not found")
     return data
 
 @app.post("/")
